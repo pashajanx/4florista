b7dfc5dafdb4a0bc49d96abd4232b228a05e2624
Ilya-web <apolokhovilya@gmail.com>
2016-11-10 15:30:56 +0200

    три стриницы в папке page

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+	// моб.меню
 	$(".toggle-mnu_1").click(function() {
 	  $(this).toggleClass("on");
 	  $(".main-mnu_1").slideToggle();
@@ -10,4 +10,46 @@ $(document).ready(function () {
 	  $(".main-mnu_2").slideToggle();
 	  return false;
 	});
+
+	// ховер на хнопки
+	$(function() {
+	    $('.btn')
+	        .on('mouseenter', function(e) {
+	            var parentOffset = $(this).offset(),
+	                relX = e.pageX - parentOffset.left,
+	                relY = e.pageY - parentOffset.top;
+	            $(this).find('span').css({ top: relY, left: relX })
+	        })
+	        .on('mouseout', function(e) {
+	            var parentOffset = $(this).offset(),
+	                relX = e.pageX - parentOffset.left,
+	                relY = e.pageY - parentOffset.top;
+	            $(this).find('span').css({ top: relY, left: relX })
+	        });
+	    $('[href=#]').click(function() {
+	        return false });
+	});
+
+
+	//Fixed Cart
+	var scrollTop, wrapperBasketTop;
+	function fixedCart() {
+		scrollTop = $(window).scrollTop();
+		wrapperBasketTop = $('.header_top').outerHeight();
+		if(scrollTop >= wrapperBasketTop){
+			$('.wrapper_basket').addClass('fixed-style');
+		}else{
+			$('.wrapper_basket').removeClass('fixed-style');
+		}
+	}
+
+	$(window).load(function () {
+		fixedCart();
+	});
+
+	$(window).on('scroll', function () {
+		fixedCart();
+	});
+
+
 });
